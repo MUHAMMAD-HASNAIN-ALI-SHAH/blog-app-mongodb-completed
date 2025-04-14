381c1455e40914b06cfaf7efea046c5db9f19f2c
MUHAMMAD HASNAIN ALI SHAH <161144540+MUHAMMAD-HASNAIN-ALI-SHAH@users.noreply.github.com>
2025-04-13 23:54:37 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-app-hasnain-frontend.vercel.app/",
     credentials: true,
   })
 );
